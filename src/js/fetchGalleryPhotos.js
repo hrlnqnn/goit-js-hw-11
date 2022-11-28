c2aa4536065c0b89ff22fc63d8da6c257f94f451
Anastasiia Sukhorukova <hrlnqnn@gmail.com>
2022-11-28 20:20:57 +0200

    Update fetchGalleryPhotos.js

--- src/js/fetchGalleryPhotos.js
+++ src/js/fetchGalleryPhotos.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function fetchGalleryPhotos(searchValue, currentPage = 1, perPage = 40) {
   const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '30819476-f9247b8148545f328aa332668';
+  const API_KEY = '31530032-bcbc50fc25a9a5255ae59ed3d';
 
   const searchParams = new URLSearchParams({
     key: API_KEY,
